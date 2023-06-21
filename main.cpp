d7fb0bdb20a6d902f8d23daed163a93870267641
Joselyn Moncayo <joselynmoncayo1999@gmail.com>
2023-06-21 15:11:08 +0000

    ejemplo01

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <mpi.h>
+
+int main (int argc, char** argv){
+
+    MPI_Init(&argc, &argv);
+
+    int rank, nprocs;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs); 
+
+if(rank==0){
+    std::printf("MPI group size: %d\n", nprocs);
+}else{
+    std::printf("Soy el rank %d\n", rank);
+}
+    
+
+   // while(1);
+    MPI_Finalize();
+    return 0;
+}
